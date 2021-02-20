4655be45792b09ca383112ea0e0964c68c172467
maomincoding <17864296568@163.com>
2021-02-20 23:34:20 +0800

    2021.2.20 23:34

--- demo15/demo15.ts
+++ demo15/demo15.ts
@@ -1,12 +1,21 @@
-function join<ABC>(a:ABC, b:ABC) {
+// 泛型 泛指的类型
+//1.
+function join<T>(a:T, b:T) {
   return `${a}${b}`;
 }
 
 join<string>('1','2');
 
-
-function join1<ABC>(params:Array<ABC>) {
+//2.
+function join1<T>(params:T[]) {
   return params;
 }
 
 join1<string>(['123']);
+
+//3.
+function join2<T,P>(a: T, b: P) {
+  return `${a}${b}`;
+}
+
+join2<number,string>(1,'2');
