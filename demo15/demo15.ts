247120b60dc9f5d4a9fb241440cb3e30abc93e47
maomincoding <17864296568@163.com>
2021-03-04 21:30:57 +0800

    2021.3.4 21:30

--- demo15/demo15.ts
+++ demo15/demo15.ts
@@ -1,21 +1,30 @@
 // 泛型 泛指的类型
-//1.
-function join<T>(a:T, b:T) {
-  return `${a}${b}`;
+//1. 标准
+function join<T>(a: T, b: T) {
+    return `${a}${b}`;
 }
 
-join<string>('1','2');
+join<string>('1', '2');
 
-//2.
-function join1<T>(params:T[]) {
-  return params;
+//2. 数组泛型
+function join1<T>(params: T[]) {
+    return params;
 }
 
 join1<string>(['123']);
 
-//3.
-function join2<T,P>(a: T, b: P) {
-  return `${a}${b}`;
+//3. 多个泛型
+function join2<T, P>(a: T, b: P) {
+    return `${a}${b}`;
 }
 
-join2<number,string>(1,'2');
+join2<number, string>(1, '2');
+
+//4. 返回泛型
+function join3<T>(a: T ):T {
+    return a;
+}
+
+join3<string>( '2');
+
+// 位置：在函数括号前面写<T>
